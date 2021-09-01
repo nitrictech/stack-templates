84a59e708ffdbf879b5ac342fb9eb54b079ab3ba
Jye Cusch <jye.cusch@nitric.io>
2021-09-01 15:20:58 +1000

    feat: initial python stack template

---
+++ python-service/functions/read.py
@@ -0,0 +1,32 @@
+from nitric.api.exception import NotFoundException, NitricServiceException
+from nitric.faas import start, Trigger, Response
+from nitric.api import Documents
+
+
+async def handler(trigger: Trigger) -> Response:
+    ctx = trigger.context.as_http()
+    response = trigger.default_response()
+
+    # get id param from HTTP request path
+    try:
+        doc_id = ctx.path.split("/")[1]
+    except IndexError:
+        response.data = "Invalid request"
+        response.context.as_http().status = 400
+        return response
+
+    try:
+        example = await Documents().collection("example").doc(doc_id).get()
+        response.data = example
+    except NotFoundException:
+        response.context.as_http().status = 404
+        response.data = "Example not found"
+    except NitricServiceException:
+        response.context.as_http().status = 500
+        response.data = "An unexpected error occurred"
+
+    return response
+
+
+if __name__ == "__main__":
+    start(handler)

36e1596c628409ae65c527957758a22568bd9b53
medgar <malcolm.edgar@nitric.io>
2021-09-30 20:44:10 +1000

    feat: updated java-stack to support Java Context API

---
+++ java-stack/functions/create/src/main/java/com/example/service/CreateHandler.java
@@ -0,0 +1,57 @@
+package com.example.service;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.example.service.middleware.LoggerMiddleware;
+import com.example.service.model.Example;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.nitric.api.document.Documents;
+import io.nitric.faas.Faas;
+import io.nitric.faas.http.HttpContext;
+import io.nitric.faas.http.HttpHandler;
+
+/**
+ * Provides an example document CreateHandler function.
+ */
+public class CreateHandler implements HttpHandler {
+
+    final Documents documents;
+
+    public CreateHandler(Documents documents) {
+        this.documents = documents;
+    }
+
+    @Override
+    public HttpContext handle(HttpContext context) {
+        try {
+            var json = context.getRequest().getDataAsText();
+            var example = new ObjectMapper().readValue(json, Example.class);
+            var id = UUID.randomUUID().toString();
+
+            documents.collection("examples").doc(id, Example.class).set(example);
+
+            var msg = String.format("Created example with ID: %s", id);
+            context.getResponse().data(msg);
+
+        } catch (IOException ioe) {
+            context.getResponse()
+                .status(500)
+                .data("error: " + ioe);
+        }
+
+        return context;
+    }
+
+    public static void main(String[] args) {
+        var loggerMiddleware = new LoggerMiddleware();
+        var createHandler = new CreateHandler(new Documents());
+
+        new Faas()
+            .addMiddleware(loggerMiddleware)
+            .http(createHandler)
+            .start();
+    }
+
+}
---
+++ java-stack/functions/list/src/main/java/com/example/service/ListHandler.java
@@ -0,0 +1,65 @@
+package com.example.service;
+
+import com.example.service.middleware.LoggerMiddleware;
+import com.example.service.model.Example;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.nitric.api.document.Documents;
+import io.nitric.api.document.ResultDoc;
+import io.nitric.faas.Faas;
+import io.nitric.faas.http.HttpContext;
+import io.nitric.faas.http.HttpHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Provides an example document ListHandler function.
+ */
+public class ListHandler implements HttpHandler {
+
+    final Documents documents;
+
+    public ListHandler(Documents documents) {
+        this.documents = documents;
+    }
+
+    @Override
+    public HttpContext handle(HttpContext context) {
+        Stream<ResultDoc<Example>> stream = documents.collection("examples")
+            .query(Example.class)
+            .stream();
+
+        List<Example> examples = stream
+             .map(ResultDoc::getContent)
+             .collect(Collectors.toUnmodifiableList());
+
+        try {
+            var json = new ObjectMapper().writeValueAsString(examples);
+
+            context.getResponse()
+                .addHeader("Content-Type", "application/json")
+                .data(json);
+
+        } catch (IOException ioe) {
+            context.getResponse()
+                .status(500)
+                .data("Error querying examples: " + ioe.toString());
+        }
+
+        return context;
+    }
+
+    public static void main(String[] args) {
+        var loggerMiddleware = new LoggerMiddleware();
+        var listHandler = new ListHandler(new Documents());
+
+        new Faas()
+            .addMiddleware(loggerMiddleware)
+            .http(listHandler)
+            .start();
+    }
+
+}
